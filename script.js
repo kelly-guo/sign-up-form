eafcc0988c4ea4c75dc91f6171c39aa907146666
Kelly <kellyguo1101@gmail.com>
2026-09-03 11:21:48 -0700

    password validation

---
+++ script.js
@@ -0,0 +1,23 @@
+/*const form = document.getElementById("form");
+
+function validatePasswords(event) {
+    const passInput = document.getElementById("pass");
+    const confirmPass = document.getElementById("confirm-pass");
+
+    const password1 = passInput.value;
+    const password2 = confirmPass.value;
+
+    if (password1!==password2){
+        event.preventDefault();
+
+        confirmPass.setCustomValidity("Invalid Password");
+        confirmPass.reportValidity();
+
+    } else {
+        confirmPass.setCustomValidity("");
+    }
+
+
+}
+
+form.addEventListener("submit", formSubmit);*/
